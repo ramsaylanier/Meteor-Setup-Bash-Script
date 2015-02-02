53a6d9e2387eb95ad4d34bf8095b9b12c48760c7
Ramsay Lanier <ramsay@nclud.com>
2015-02-01 19:51:02 -0500

    init
    Signed-off-by: Ramsay Lanier <ramsay@nclud.com>

---
+++ meteor_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+clear
+echo "Knowledge is Power"
+echo $HOME
+echo $PWD
+
+# TRACES="/Traces"
+
+meteor remove autopublish
+meteor remove insecure
+meteor add iron:router
+
+CLIENT=$PWD'/client'
+SERVER=$PWD'/server'
+LIB=$PWD'/lib'
+PUBLIC=$PWD'/public'
+ASSETS=$PWD'/client/assets'
+CSS=$ASSETS'/css'
+SCSS=$ASSETS'/scss'
+COLLECTIONS=$LIB'/collections'
+VIEWS=$CLIENT'/views'
+
+touch $LIB'/router.js'
+touch $PWD'/settings.json'
+
+folders=($CLIENT $SERVER $LIB $PUBLIC $ASSETS $CSS $SCSS $COLLECTIONS $VIEWS)
+for folder in "${folders[@]}"
+ do
+   mkdir $folder
+ done
